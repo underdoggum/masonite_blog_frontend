ebbe792d8fde91a1fb5afcbe0fa94aa6ecfda682
Nathan Noack <nnoack3@gmail.com>
2021-12-06 17:26:27 -0500

    added netlify CI build script fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders Create New Blog Button", () => {
+  render(
+    <Router>
+      <Route component={App} />
+    </Router>
+  );
+  const linkElement = screen.getByText(/Create New Blog/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("create button takes you to create form", () => {
+  render(
+    <Router>
+      <Route component={App} />
+    </Router>
+  );
+  const linkElement = screen.getByText(/Create New Blog/i);
+  fireEvent.click(linkElement);
+  const createButton = screen.getByText(/Create Blog/i);
+  expect(createButton).toBeInTheDocument();
 });
